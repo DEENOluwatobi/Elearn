f999267a4d5fa6c1040d5f5ef8face16a726c539
DEENOluwatobi <shodimutobie@gmail.com>
2023-10-10 14:30:38 +0100

    First commit

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 left-0 w-full h-10 flex justify-between items-center px-2 py-1'>
+        <div>
+            Logo
+        </div>
+
+        <ul>
+            <li>HOME</li>
+            <li>ADMISSION</li>
+            <li>SKILL</li>
+            <li>ABOUT</li>
+
+        </ul>
+
+        <div>
+            <button>Login</button>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Navbar
