2de49aac9d931bc024928b2c4d683a3b8c00954d
DEENOluwatobi <shodimutobie@gmail.com>
2023-10-14 20:41:06 +0100

    leading-10

--- src/app/Components/HeroSection.tsx
+++ src/app/Components/HeroSection.tsx
@@ -12,16 +12,16 @@ const HeroSection = () => {
 
           <div className='w-full md:w-[40%]'>
             <div className='flex flex-col justify-center md:justify-start items-center md:items-start'>
-              <span className={`poppins text-black font-bold text-[5em] primary leading-[2px]`}>Watch.</span>
-              <span className={`poppins text-black font-bold text-[5em] primary leading-[2px]`}>Learn.</span>
-              <span className={`poppins text-black font-bold text-[5em] primary leading-[2px]`}>Grow.</span>
+              <span className={`poppins text-black font-bold text-[5em]  leading-[10px]`}>Watch.</span>
+              <span className={`poppins text-black font-bold text-[5em]  leading-[10px]`}>Learn.</span>
+              <span className={`poppins text-black font-bold text-[5em]  leading-[10px]`}>Grow.</span>
             </div>
 
-            <div className='border border-gray-400 w-full h-10 rounded-lg flex overflow-hidden'>
+            <div className='border border-gray-400 w-full h-12 rounded-xl flex overflow-hidden'>
               <input 
                 type="text" 
                 placeholder='Search courses...'
-                className='border-none text-black outline-none placeholder:text-gray-300 text-sm w-full h-full px-2 py-1'
+                className='border-none text-black outline-none placeholder:text-gray-400 text-sm w-full h-full p-2'
               />
               <button 
                 type='submit' 
