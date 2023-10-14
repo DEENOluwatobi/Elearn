2787a7f0a789cafc9044eb8ac3d89fbfa87579a7
DEENOluwatobi <shodimutobie@gmail.com>
2023-10-14 21:35:02 +0100

    icons, text  edit

--- base/icons.tsx
+++ base/icons.tsx
@@ -2145,9 +2145,9 @@ export const InstagramLine = ({ size = "original", className }: iconProp) => {
   );
 };
   
-export const GoogleLine = ({ size = "original", className }: iconProp) => {
+export const MailLine = ({ size = "original", className }: iconProp) => {
   return ( 
-    <svg xmlns="http://www.w3.org/2000/svg" className={`${sizes[size]} ${className ? className : ""}`} width="25" height="25" preserveAspectRatio="xMidYMid" viewBox="0 0 25 25" id="google"><path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"></path><path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"></path><path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"></path><path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" className={`${sizes[size]} ${className ? className : ""}`} width="24" height="24" viewBox="0 0 24 24" id="mail"><path fill="none" d="M0 0h24v24H0V0z"></path><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-1 14H5c-.55 0-1-.45-1-1V8l6.94 4.34c.65.41 1.47.41 2.12 0L20 8v9c0 .55-.45 1-1 1zm-7-7L4 6h16l-8 5z"></path></svg>
   );
 };
 
--- src/app/Components/HeroSection.tsx
+++ src/app/Components/HeroSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTheme } from '@context/ThemeContext';
 import Gallery from './Gallery';
-import { FacebookLine, GoogleLine, InstagramLine, TwitterLine } from '@base/icons';
+import { FacebookLine, MailLine, InstagramLine, TwitterLine } from '@base/icons';
 
 
 const HeroSection = () => {
@@ -12,11 +12,11 @@ const HeroSection = () => {
       <section className="hero bg-primary">
         <div className="relative px-4 flex gap-4 flex-col md:flex-row w-full h-full justify-center items-center z-10">
 
-          <div className='w-full md:w-[40%]'>
+          <div className='w-full md:w-[40%] flex flex-col justify-between py-2'>
             <div className='flex flex-col justify-center md:justify-start items-center md:items-start'>
-              <span className={`poppins text-black font-bold text-[5em]  leading-[30px]`}>Watch.</span>
-              <span className={`poppins text-black font-bold text-[5em]  leading-[30px]`}>Learn.</span>
-              <span className={`poppins text-black font-bold text-[5em]  leading-[30px]`}>Grow.</span>
+              <span className={`poppins font-bold text-[5em] text-green-700 leading-[60px]`}>Watch.</span>
+              <span className={`poppins font-bold text-[5em] text-red-600 leading-[60px]`}>Learn.</span>
+              <span className={`poppins font-bold text-[5em] text-yellow-600 leading-[60px]`}>Grow.</span>
             </div>
 
             <div className='border border-gray-400 w-full h-12 rounded-xl flex overflow-hidden'>
@@ -38,19 +38,19 @@ const HeroSection = () => {
 
           <div className='w-full md:w-[60%] h-full overflow-hidden flex gap-3 justify-center items-center'>
             <Gallery/>
-            <div className="flex items-center justify-center flex-col">
-              <div className="w-3 h-30 bg-gray-700"></div>
-              <div className="w-50 h-50 flex items-center justify-center rounded-full border-3 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-blue-400 hover:border-gray-500">
+            <div className="flex items-center justify-center flex-col w-12 h-full gap-4">
+              <div className="w-3 h-8 bg-gray-700"></div>
+              <div className="w-5 h-5 flex items-center justify-center rounded-full border-2 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-blue-400 hover:border-gray-500">
                 <FacebookLine/>
               </div>
-              <div className="w-50 h-50 flex items-center justify-center rounded-full border-3 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-blue-500 hover:border-gray-500">
+              <div className="w-5 h-5 flex items-center justify-center rounded-full border-2 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-blue-500 hover:border-gray-500">
                 <InstagramLine/>
               </div>
-              <div className="w-50 h-50 flex items-center justify-center rounded-full border-3 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-gradient-to-br from-blue-400 via-red-400 to-green-400 hover:border-gray-500">
+              <div className="w-5 h-5 flex items-center justify-center rounded-full border-2 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-gradient-to-br from-blue-400 via-red-400 to-green-400 hover:border-gray-500">
                 <TwitterLine/>
               </div>
-              <div className="w-50 h-50 flex items-center justify-center rounded-full border-3 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-red-400 hover:border-gray-500">
-                <GoogleLine/>
+              <div className="w-5 h-5 flex items-center justify-center rounded-full border-2 border-gray-700 cursor-pointer transition duration-500 ease-in-out hover:bg-red-400 hover:border-gray-500">
+                <MailLine/>
               </div>
               <div className="w-3 h-30 bg-gray-700"></div>
             </div>
